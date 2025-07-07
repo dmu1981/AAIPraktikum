012c4cbcaeb168de206b0faf235aebe245cbc0f6
Dennis Müller <dennis.mueller@hs-duesseldorf.de>
2025-07-07 19:21:25 +0200

    Adversarial loss converges for the very first time

--- adversarialloss/misc.py
+++ adversarialloss/misc.py
@@ -1,5 +1,72 @@
 
 import torch
+from torch import nn
+from torchvision.models import vgg16
+
+class VGG16PerceptualLoss(nn.Module):
+    def __init__(self):
+        """Initialize the VGG16 perceptual loss model.
+        
+        It computes the perceptual loss as the mean squared error between the features.
+        
+        The model is set to evaluation mode and the parameters are frozen.
+        
+        **TODO**:
+
+        - Load the VGG16 model with pretrained weights. Use `torchvision.models.vgg16(pretrained=True)`.        
+
+        - Restrict the VGG16 model to the first 16 layers by using `self.vgg = vgg16(pretrained=True).features[:16]`.
+
+        - Set the model to evaluation mode using `.eval()`.
+
+        - Freeze the parameters of the VGG16 model by setting `param.requires_grad = False` for all parameters. 
+          NOTE: Iterate through all parameters by using the `self.vgg.parameters()`-Iterator.
+
+        - Initialize the L2 loss function using `nn.MSELoss()`.  
+        """
+        super(VGG16PerceptualLoss, self).__init__()
+        self.vgg = vgg16(pretrained=True).features[:16].eval().cuda()
+
+        for param in self.vgg.parameters():
+            param.requires_grad = False
+
+        self.l1_loss = nn.L1Loss()
+
+    def forward(self, output, target):
+        """Compute the perceptual loss between two images.
+        
+        Parameters:
+        -----------
+            output (torch.Tensor): 
+              The output image tensor from the upscaler network.
+
+            target (torch.Tensor): 
+              The target image tensor from ground truth.
+
+        Returns:
+        --------  
+            torch.Tensor: 
+              The computed perceptual loss as the mean squared error between the features of the two images.
+
+        **TODO**:
+
+        - Resize `output` and `target` to 224x224 using `torch.nn.functional.interpolate()`. Use `mode='bilinear'` and `align_corners=False`. 
+        
+        - Pass `output` through the VGG16 model to get the features `f1`.
+
+        - Pass `target` through the VGG16 model to get the features `f2`. Note: You should use `torch.no_grad()` to avoid computing gradients for the target image.
+
+        - Compute and return the L2 loss between `f1` and `f2` using `self.l2_loss(f1, f2)`.
+        """
+        f1 = self.vgg(output)
+
+        with torch.no_grad():
+          f2 = self.vgg(target)
+
+        return self.l1_loss(f1, f2)
+
+
+import torch
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 from PIL import Image
@@ -70,7 +137,7 @@ def get_dataloader(inputSize=128, outputSize=256, batch_size=32):
     return dataloader
 
 class ResNetBlock(nn.Module): 
-    def __init__(self, in_channels, out_channels, kernel_size=9, padding=None):
+    def __init__(self, in_channels, out_channels, kernel_size=9, padding=None, stride=1, norm=True):
         """Initialisiert einen ResNet-Block mit zwei Convolutional-Schichten, Batch-Normalisierung und ReLU-Aktivierung.
         
         Parameters:
@@ -92,16 +159,16 @@ class ResNetBlock(nn.Module):
         if padding is None:
             padding = (kernel_size - 1) // 2
             
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding, bias=False)
-        self.bn1 = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU(inplace=True)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding, bias=False, stride=stride)
+        self.bn1 = nn.BatchNorm2d(out_channels) if norm else nn.Identity() #nn.InstanceNorm2d(out_channels)
+        self.relu = nn.LeakyReLU(inplace=True)
 
         self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, stride=1, padding=padding, bias=False)
-        self.bn2 = nn.BatchNorm2d(out_channels)
-        
-        if in_channels != out_channels:
+        self.bn2 = nn.BatchNorm2d(out_channels) if norm else nn.Identity() #nn.InstanceNorm2d(out_channels)
+
+        if stride != 1 or in_channels != out_channels:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False),
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False, stride=stride),
             )
         else:
             self.shortcut = nn.Identity()
@@ -109,10 +176,10 @@ class ResNetBlock(nn.Module):
     def forward(self, x):
         residual = self.shortcut(x)
         out = self.conv1(x)
-        out = self.bn1(self.relu(out))
+        out = self.relu(self.bn1(out))
         out = self.conv2(out)
         out = out + residual
-        return self.bn2(self.relu(out))
+        return self.relu(self.bn2(out))
 
 def save_checkpoint(model, optimizer, epoch, filename="checkpoint.pth"):
     """Speichert den aktuellen Zustand des Modells und des Optimierers in einer Datei."""
@@ -140,15 +207,21 @@ def load_checkpoint(model, optimizer, filename="checkpoint.pth"):
         return 0
 
 
-def log_metrics(writer, epoch, total_loss, total_lips, total_mse, total_psnr, total_cnt):
-    avg_loss = total_loss / total_cnt
+def log_metrics(writer, epoch, total_lips, total_mse, total_psnr, total_loss_critic_real, total_loss_critic_fake, total_generator_loss, total_content_loss, total_cnt):
     avg_lips = total_lips / total_cnt
     avg_mse = total_mse / total_cnt
     avg_psnr = total_psnr / total_cnt
-    writer.add_scalar('Loss', 1000.0 * avg_loss, epoch)
     writer.add_scalar('LPIPS', 1000.0 * avg_lips, epoch)
     writer.add_scalar('MSE', 1000.0 * avg_mse, epoch)
     writer.add_scalar('PSNR', avg_psnr, epoch)
+    writer.add_scalar('Critic Real', 1000.0 * total_loss_critic_real / total_cnt, epoch)
+    writer.add_scalar('Critic Fake', 1000.0 * total_loss_critic_fake / total_cnt, epoch)
+
+    loss_C = total_loss_critic_fake - total_loss_critic_real
+    writer.add_scalar('loss_C', 1000.0 * loss_C / total_cnt, epoch)
+
+    writer.add_scalar('Generator Loss', 1000.0 * total_generator_loss / total_cnt, epoch)
+    writer.add_scalar('Content Loss', 1000.0 * total_content_loss / total_cnt, epoch)
 
 def log_images(writer, model, dataloader, epoch):
     model.eval()
@@ -193,46 +266,3 @@ class PSNR(nn.Module):
         mse = F.mse_loss(output, target)
         psnr = 20 * torch.log10(self.max_val / torch.sqrt(mse))
         return psnr
-
-def train(prefix, model, dataloader, loss_fn):
-  print(f"Training {prefix} model...")
-
-  optim = torch.optim.Adam(model.parameters(), lr=0.001)
-  metric = lpips.LPIPS(net='vgg').cuda()  # Using SqueezeNet for perceptual loss
-  mseMetric = nn.MSELoss()
-  psnrMetric = PSNR()
-
-  ep = load_checkpoint(model, optim, filename=f"{prefix}.pt")
-
-  writer = SummaryWriter(f"runs/{prefix}")
-
-  for epoch in range(ep, ep + 30):
-      total_loss = 0.0
-      total_lips = 0.0
-      total_mse = 0.0
-      total_psnr = 0.0
-      total_cnt = 0
-      bar = tqdm(dataloader)
-      for batch in bar:
-          input, target = batch
-          input = input.cuda()
-          target = target.cuda()
-
-          optim.zero_grad()
-          output = model(input)
-          loss = loss_fn(output, target)
-          loss.backward()
-          optim.step()
-
-          total_loss += loss.item()
-          total_cnt += input.size(0)
-
-          total_lips += metric(2.0 * output - 1.0, 2.0 * target-1.0).mean().item()
-          total_mse += mseMetric(output, target).item()
-          total_psnr += psnrMetric(output, target).item()
-
-          bar.set_description(f"[{epoch+1}], Loss: {1000.0 * total_loss / total_cnt:.3f}, LPIPS: {1000.0 * total_lips / total_cnt:.3f}, MSE: {1000.0 * total_mse / total_cnt:.3f}, PSNR: {total_psnr / total_cnt:.3f}")
-
-      log_metrics(writer, epoch + 1, total_loss, total_lips, total_mse, total_psnr, total_cnt)
-      log_images(writer, model, dataloader, epoch + 1)
-      save_checkpoint(model, optim, epoch + 1, filename=f"{prefix}.pt")
