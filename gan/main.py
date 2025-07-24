24c14afa2861ba69321303086bb33a45aa644693
Dennis Müller <dennis.mueller@hs-duesseldorf.de>
2025-07-24 20:29:03 +0200

    GAN converges but really bad quality

---
+++ gan/main.py
@@ -0,0 +1,325 @@
+import torch
+from torch import nn
+from torchvision.models import vgg16
+import torch.nn.init as init
+import torch.nn.functional as F
+from misc import (
+    get_dataloader,
+    ResNetBlock,
+    train,
+    TVLoss,
+    #Critic
+)
+from tqdm import tqdm
+from torch.utils.tensorboard import SummaryWriter
+
+
+
+class Critic4(nn.Module):
+    def __init__(self, input_channels=3, mid_channels=1024):
+        super(Critic4, self).__init__()
+
+        self.midChannels = mid_channels
+        self.down = nn.Sequential(
+          nn.Conv2d(input_channels, self.midChannels, kernel_size=4, stride=1, padding=0),
+          nn.LeakyReLU(inplace=True)
+        )
+
+        self.fc = nn.Sequential(
+          nn.Linear(self.midChannels, self.midChannels // 2),
+          nn.LeakyReLU(inplace=True),
+          nn.Linear(self.midChannels // 2, 1),
+        )
+
+
+    def forward(self, x):
+        x = self.down(x)
+        x = self.fc(x.view(-1, self.midChannels))
+
+        return x
+    
+class Critic(nn.Module):
+    def __init__(self, channels_img, features_d):
+        super(Critic, self).__init__()
+        self.disc = nn.Sequential(
+            nn.Conv2d(channels_img, features_d, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2),
+            self._block(features_d, features_d * 2, 4, 2, 1),
+            self._block(features_d * 2, features_d * 4, 4, 2, 1),
+            self._block(features_d * 4, features_d * 8, 4, 2, 1),
+            nn.Conv2d(features_d * 8, 1, kernel_size=4, stride=2, padding=0),
+        )
+
+    def _block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.Conv2d(
+                in_channels,
+                out_channels,
+                kernel_size,
+                stride,
+                padding,
+                bias=False
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(0.2)
+        )
+
+    def forward(self, x):
+        return self.disc(x)
+
+def initialize_weights(model):
+    classname = model.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(model.weight.data, 0.0, 0.002)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(model.weight.data, 1.0, 0.02)
+        nn.init.constant_(model.bias.data, 0)
+
+# class UpsampleBlock(nn.Module):
+#     def __init__(self, mid_channels):
+#         super(UpsampleBlock, self).__init__()
+
+#         self.up = nn.Sequential(
+#             nn.Conv2d(3, mid_channels, kernel_size=5, padding=2),
+#             #nn.BatchNorm2d(mid_channels * 4),
+#             nn.LeakyReLU(inplace=True),
+#             nn.Conv2d(mid_channels, mid_channels * 4, kernel_size=5, padding=2),
+#             #nn.BatchNorm2d(out_channels * 4),
+#             nn.LeakyReLU(inplace=True),
+#             nn.PixelShuffle(upscale_factor=2),
+#             nn.Conv2d(mid_channels, 3, kernel_size=5, padding=2),
+#             )
+
+#     def forward(self, x):
+#         x = self.up(x)
+#         return x
+
+# class LinearNetwork(nn.Module):
+#     def __init__(self, input_size, output_size, n_layers=4):
+#         super(LinearNetwork, self).__init__()
+#         self.input_size = input_size
+        
+#         layers = [self._layer(input_size, input_size) for _ in range(n_layers - 1)]
+#         layers.append(self._layer(input_size, output_size))
+#         self.inputNetwork = nn.Sequential(
+#             *layers
+#         )
+
+#     def _layer(self, input_size, output_size):
+#         return nn.Sequential(
+#             nn.Linear(input_size, output_size, bias=False),
+#             nn.BatchNorm1d(output_size),
+#             nn.LeakyReLU(inplace=True)
+#         )
+
+#     def forward(self, x):
+#         x = x.reshape(-1, self.input_size)
+#         x = self.inputNetwork(x)
+#         return x 
+
+class Generator(nn.Module):
+    def __init__(self, z_dim, channels_img, features_g):
+        super(Generator, self).__init__()
+        self.net = nn.Sequential(
+            self._block(z_dim, features_g * 16, 4, 1, 0),
+            self._block(features_g * 16, features_g * 8, 4, 2, 1),
+            self._block(features_g * 8, features_g * 4, 4, 2, 1),
+            self._block(features_g * 4, features_g * 2, 4, 2, 1),
+            nn.ConvTranspose2d(
+                features_g * 2, channels_img, kernel_size=4, stride=2, padding=1
+            ),
+            #nn.Tanh()
+        )
+
+    def _block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.ConvTranspose2d(
+                in_channels,
+                out_channels,
+                kernel_size,
+                stride,
+                padding,
+                bias=False
+            ),
+            #nn.BatchNorm2d(out_channels),
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+        return self.net(x)
+    
+class GeneratorLoss(nn.Module):
+    def __init__(self, critic):
+        super(GeneratorLoss, self).__init__()
+        self.tvLoss = TVLoss()
+        self.critic = critic
+
+    def forward(self, img, epoch):
+        #self.critic.eval()
+        adversarial_loss = self.critic(img).mean()
+
+        adversarial_lambda = 1.0# min(1.0, epoch / 5.0)
+
+        content_loss = 10.0 * self.tvLoss(img)
+
+        return {
+            "generator_loss": content_loss - adversarial_lambda * adversarial_loss,
+            #"generator_loss": -adversarial_lambda * adversarial_loss,
+            "content_loss": content_loss,
+            "adversarial_loss": adversarial_loss,
+        }
+
+
+
+class CriticLoss(nn.Module):
+    def __init__(self, critic):
+        super(CriticLoss, self).__init__()
+        self.critic = critic
+
+    def compute_gradient_penalty(self, realTuple, fakeTuple, lambda_gp=10):
+        # Generate random interpolation between real and fake images
+
+        batch_size = realTuple[0].size(0)
+        epsilon = torch.rand(batch_size, 1, 1, 1, device=realTuple[0].device)
+
+        realTuple = (realTuple[0],)
+        fakeTuple = (fakeTuple[0],)
+        interpolated = [(epsilon * real + (1 - epsilon) * fake).requires_grad_(True) for real, fake in zip(realTuple, fakeTuple)]
+
+        # Compute the critic's output for the interpolated images
+        critic_output = self.critic(interpolated[0])
+
+        # Compute the gradients of the critic's output with respect to the interpolated images
+        grad_outputs = torch.ones_like(critic_output)
+
+        gradients = torch.autograd.grad(
+            outputs=critic_output,
+            inputs=interpolated,
+            grad_outputs=grad_outputs,
+            create_graph=True,
+            retain_graph=True,
+            only_inputs=True,
+        )[0]
+
+        # Compute the gradient penalty
+        gradients = gradients.view(batch_size, -1)
+        gradient_norm = gradients.norm(2, dim=1)
+        gradient_penalty = ((gradient_norm - 1) ** 2).mean() * lambda_gp
+
+        # Return the gradient penalty and the gradient norm for logging purposes
+        return gradient_penalty, gradient_norm
+
+    def forward(self, real, fake):
+        gp, gradient_norm = self.compute_gradient_penalty(real, fake)
+        #self.critic.train()
+
+        loss_c = -self.critic(real[0]).mean() + self.critic(fake).mean()
+
+        return {
+            "loss_c": loss_c + gp,
+            "gradient_norm": gradient_norm,
+            "pure_wgan_loss": loss_c,
+        }
+
+
+class UpscaleTrainer:
+    def __init__(self):
+        self.criticUpdates = 0
+        self.generator = Generator(100, 3, 64).cuda()
+        self.critic = Critic(3, 64).cuda()
+
+        initialize_weights(self.generator)
+        initialize_weights(self.critic)
+
+        self.generatorLoss = GeneratorLoss(self.critic).cuda()
+        self.criticLoss = CriticLoss(self.critic).cuda()
+
+        self.optimGenerator = torch.optim.Adam(self.generator.parameters(), lr=1e-4, betas=(0.5, 0.999))
+        self.optimCritic = torch.optim.Adam(self.critic.parameters(), lr=1e-4, betas=(0.5, 0.999))
+
+        # Count and print parameters
+        gen_params = sum(p.numel() for p in self.generator.parameters())
+        critic_params = sum(p.numel() for p in self.critic.parameters())
+        print(f"Generator parameters: {gen_params:,}")
+        print(f"Critic parameters: {critic_params:,}")
+
+    def generate(self, batch_size):
+        noise = torch.randn((batch_size, 100, 1, 1), device="cuda")
+        return self.generator(noise)
+
+    def train_critic(self, fake, target):
+        self.optimCritic.zero_grad()
+        result = self.criticLoss(target, fake)
+        critic_loss, gradient_norm, loss_c = (
+            result["loss_c"],
+            result["gradient_norm"],
+            result["pure_wgan_loss"],
+        )
+        critic_loss.backward()
+        #torch.nn.utils.clip_grad_norm_(self.critic.parameters(), max_norm=150.0)
+        self.optimCritic.step()
+
+        return {
+            "gradient_norm": gradient_norm.mean().item(),
+            "loss_c": loss_c.item(),
+        }
+
+    def train_generator(self, batch_size, epoch):
+        self.optimGenerator.zero_grad()
+        
+        fake = self.generate(batch_size)
+
+        result = self.generatorLoss(fake, epoch)
+        loss, content_loss, adversarial_loss = (
+            result["generator_loss"],
+            result["content_loss"],
+            result["adversarial_loss"],
+        )
+
+        loss.backward()
+
+        #torch.nn.utils.clip_grad_norm_(self.generator.parameters(), max_norm=150.0)
+        gen_norm = torch.nn.utils.clip_grad_norm_(
+            self.generator.parameters(), max_norm=1e9
+        )
+
+        self.optimGenerator.step()
+
+        return {
+            "loss": loss.item(),
+            "content_loss": content_loss.item(),
+            "adversarial_loss": adversarial_loss.mean().item(),
+            "gradient_norm": gen_norm.item(),
+            "output": fake.detach() if fake is not None else None,
+        }
+
+    def train_batch(self, target, epoch):
+        # Train Critic every step
+        batch_size = target[0].size(0)
+        
+        
+        for _ in range(5):
+          # Train Critic
+          fake = self.generate(batch_size)
+          scoresCritic = self.train_critic(fake, target)
+  
+        self.criticUpdates += 1
+
+        # Train Generator only every 4th step
+        if self.criticUpdates == 5 or True:
+            scoresGenerator = self.train_generator(batch_size, epoch)
+            self.criticUpdates = 0
+        else:
+            scoresGenerator = None
+
+        return {"critic": scoresCritic, "generator": scoresGenerator}
+
+
+if __name__ == "__main__":
+    prefix = "gan64"
+
+    dataloader = get_dataloader(batch_size=256)
+
+    trainer = UpscaleTrainer()
+
+    train(prefix, trainer, dataloader)
---
+++ gan/misc.py
@@ -0,0 +1,466 @@
+import torch
+from torch import nn
+from torchvision.models import vgg16
+import torchvision
+
+class VGG16PerceptualLoss(nn.Module):
+    def __init__(self):
+        """Initialize the VGG16 perceptual loss model.
+
+        It computes the perceptual loss as the mean squared error between the features.
+
+        The model is set to evaluation mode and the parameters are frozen.
+
+        **TODO**:
+
+        - Load the VGG16 model with pretrained weights. Use `torchvision.models.vgg16(pretrained=True)`.
+
+        - Restrict the VGG16 model to the first 16 layers by using `self.vgg = vgg16(pretrained=True).features[:16]`.
+
+        - Set the model to evaluation mode using `.eval()`.
+
+        - Freeze the parameters of the VGG16 model by setting `param.requires_grad = False` for all parameters.
+          NOTE: Iterate through all parameters by using the `self.vgg.parameters()`-Iterator.
+
+        - Initialize the L2 loss function using `nn.MSELoss()`.
+        """
+        super(VGG16PerceptualLoss, self).__init__()
+        self.vgg = vgg16(pretrained=True).features[:16].eval().cuda()
+
+        for param in self.vgg.parameters():
+            param.requires_grad = False
+
+        self.l1_loss = nn.L1Loss()
+
+    def forward(self, output, target):
+        """Compute the perceptual loss between two images.
+
+        Parameters:
+        -----------
+            output (torch.Tensor):
+              The output image tensor from the upscaler network.
+
+            target (torch.Tensor):
+              The target image tensor from ground truth.
+
+        Returns:
+        --------
+            torch.Tensor:
+              The computed perceptual loss as the mean squared error between the features of the two images.
+
+        **TODO**:
+
+        - Resize `output` and `target` to 224x224 using `torch.nn.functional.interpolate()`. Use `mode='bilinear'` and `align_corners=False`.
+
+        - Pass `output` through the VGG16 model to get the features `f1`.
+
+        - Pass `target` through the VGG16 model to get the features `f2`. Note: You should use `torch.no_grad()` to avoid computing gradients for the target image.
+
+        - Compute and return the L2 loss between `f1` and `f2` using `self.l2_loss(f1, f2)`.
+        """
+        f1 = self.vgg(output)
+
+        with torch.no_grad():
+            f2 = self.vgg(target)
+
+        return self.l1_loss(f1, f2)
+
+
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+from PIL import Image
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import os
+
+from tqdm import tqdm
+import lpips
+from torch.utils.tensorboard import SummaryWriter
+
+
+# Load the dataset from flowersSquared folder
+class CustomImageDataset(torch.utils.data.Dataset):
+    def __init__(self, root_dir, transforms=None):
+        self.root_dir = root_dir
+        self.transforms = transforms
+        self.image_files = [
+            f
+            for f in os.listdir(root_dir)
+            if f.lower().endswith((".png", ".jpg", ".jpeg", ".bmp", ".tiff"))
+        ]
+
+        # img_paths = [os.path.join(self.root_dir, self.image_files[idx]) for idx in range(len(self.image_files))]
+        # self.images = [Image.open(img_path).convert("RGB") for img_path in img_paths]
+
+    def __len__(self):
+        return len(self.image_files)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.root_dir, self.image_files[idx])
+        image = Image.open(img_path).convert("RGB")
+        # image = self.images[idx]
+
+        return tuple(transform(image) for transform in self.transforms)
+
+def get_dataloader(batch_size=32):
+    sizes = [64]
+    tr = []
+    for size in sizes:
+        transform = transforms.Compose(
+            [
+                transforms.Resize((size, size)),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ]
+        )
+        tr.append(transform)
+
+    root_dir = os.path.join(
+        os.path.dirname(__file__), "../flower_dataset/flowersSquared"
+    )
+
+    dataset = CustomImageDataset(
+        root_dir=root_dir,
+        transforms=tr
+    )
+
+    dataloader = DataLoader(
+        dataset, batch_size=batch_size, shuffle=True, pin_memory=True  # , num_workers=8
+    )
+
+    return dataloader
+
+
+class ResNetBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=9, padding=None, norm=True):
+        """Initialisiert einen ResNet-Block mit zwei Convolutional-Schichten, Batch-Normalisierung und ReLU-Aktivierung.
+
+        Parameters:
+        -----------
+
+        in_channels (int):
+          Anzahl der Eingabekanäle.
+
+        out_channels (int):
+            Anzahl der Ausgabekanäle.
+
+        kernel_size (int, optional):
+            Größe des Convolutional-Kernels. Standard ist 9.
+
+        padding (int, optional):
+            Padding für die Convolutional-Schichten. Standard ist None. In dem Fall wird das Padding automatisch berechnet, so dass die Ausgabe die gleiche Größe wie die Eingabe hat.
+        """
+        super(ResNetBlock, self).__init__()
+        if padding is None:
+            padding = (kernel_size - 1) // 2
+
+        self.conv1 = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            padding=padding,
+            bias=False,
+        )
+        self.bn1 = nn.BatchNorm2d(out_channels) if norm else nn.Identity()
+        self.relu = nn.ReLU(inplace=True)
+
+        self.conv2 = nn.Conv2d(
+            out_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            stride=1,
+            padding=padding,
+            bias=False,
+        )
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        if in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False),
+            )
+        else:
+            self.shortcut = nn.Identity()
+
+    def forward(self, x):
+        residual = self.shortcut(x)
+        out = self.conv1(x)
+        out = self.bn1(self.relu(out))
+        out = self.conv2(out)
+        out = out + residual
+        return self.bn2(self.relu(out))
+
+
+def save_checkpoint(checkpoint_dict, epoch, filename="checkpoint.pth"):
+    """Speichert den aktuellen Zustand des Modells und des Optimierers in einer Datei."""
+
+    checkpoint_dict = {
+        key: value.state_dict() for key, value in checkpoint_dict.items()
+    }
+
+    checkpoint_dict["epoch"] = epoch
+
+    torch.save(
+        checkpoint_dict,
+        filename,
+    )
+
+
+def load_checkpoint(checkpoint_dict, filename="checkpoint.pth"):
+    """Lädt den Zustand des Modells und des Optimierers aus einer Datei."""
+    try:
+        checkpoint = torch.load(filename, weights_only=True)
+
+        for key in checkpoint_dict:
+            if key in checkpoint:
+                checkpoint_dict[key].load_state_dict(checkpoint[key])
+            else:
+                raise KeyError(f"Key {key} not found in checkpoint.")
+
+        return checkpoint["epoch"]
+    except Exception as e:
+        print(f"Fehler beim Laden des Checkpoints {filename}: {e}")
+        print("Starte ohne gespeicherten Zustand.")
+        return 0
+
+
+def log_metrics(writer, metrics):
+    for key, value in metrics.items():
+        step = value.step
+        if value.count <= 20:
+            continue
+
+        writer.add_scalar(key, value.compute(), global_step=step)
+
+    writer.flush()
+
+
+class TVLoss(nn.Module):
+    def __init__(self):
+        super(TVLoss, self).__init__()
+
+    def forward(self, img):
+        return torch.mean(torch.abs(img[:, :, :-1, :] - img[:, :, 1:, :])) + torch.mean(
+            torch.abs(img[:, :, :, :-1] - img[:, :, :, 1:])
+        )
+
+
+# Denormalize images
+def denormalize(tensor):
+    mean = torch.tensor([0.485, 0.456, 0.406]).view(3, 1, 1).cuda()
+    std = torch.tensor([0.229, 0.224, 0.225]).view(3, 1, 1).cuda()
+    return tensor * std + mean
+
+
+def log_images(writer, model, dataloader, epoch):
+    model.eval()
+    with torch.no_grad():
+        batch = next(iter(dataloader))
+        batch = [b.cuda() for b in batch]
+
+
+        noise = torch.randn((16, 100, 1, 1), device="cuda")
+
+        sizes = [64]
+        for index in [0]:
+          output = model(noise)
+
+          # Rescale to 256x256 and stitch together
+          output_resized = torch.nn.functional.interpolate(
+              output[0:16], size=(64, 64), mode="nearest"#, align_corners=False
+          )
+
+          real_resized = torch.nn.functional.interpolate(
+              batch[index][0:16], size=(64, 64), mode="nearest"#, align_corners=False
+          )
+
+          output_norm = denormalize(output_resized[0:16]).clamp(0, 1)
+          real_norm = denormalize(real_resized[0:16]).clamp(0, 1)
+
+          # Stitch images horizontally
+          stitched = torchvision.utils.make_grid(output_norm, nrow=4, padding=2)
+          writer.add_image(f"Fake_{sizes[index]}", stitched, epoch)
+
+          stitched = torchvision.utils.make_grid(real_norm, nrow=4, padding=2)
+          writer.add_image(f"Real{sizes[index]}", stitched, epoch)
+
+#          break
+    model.train()
+
+
+class PSNR(nn.Module):
+    def __init__(self, max_val=1.0):
+        super(PSNR, self).__init__()
+        self.max_val = max_val
+
+    def forward(self, output, target):
+        mse = F.mse_loss(output, target)
+        psnr = 20 * torch.log10(self.max_val / torch.sqrt(mse))
+        return psnr
+
+
+class Metric:
+    def __init__(self, factor=1.0, abs=False):
+        self.total = 0.0
+        self.count = 0
+        self.step = 0
+        self.factor = factor
+        self.abs = abs
+
+    def state_dict(self):
+        return {
+            "total": self.total,
+            "count": self.count,
+            "step": self.step,
+            "factor": self.factor,
+            "abs": self.abs,
+        }
+
+    def load_state_dict(self, state_dict):
+        self.total = state_dict["total"]
+        self.count = state_dict["count"]
+        self.step = state_dict["step"]
+        self.factor = state_dict["factor"]
+        self.abs = state_dict["abs"]
+
+    def update(self, value):
+        self.total += value
+        self.count += 1
+        self.step += 1
+
+    def compute(self, reset=True):
+        if self.count == 0:
+            avg = 0.0
+        else:
+            avg = self.total / self.count
+
+        if reset:
+            self.total = 0.0
+            self.count = 0
+
+        if self.abs:
+            avg = abs(avg)
+
+        return self.factor * avg
+
+
+def train(prefix, trainer, dataloader):
+    print(f"Training {prefix} model...")
+
+    metric = lpips.LPIPS(net="vgg").cuda()  # Using SqueezeNet for perceptual loss
+    mseMetric = nn.MSELoss()
+    psnrMetric = PSNR(max_val=6.0)
+
+    writer = SummaryWriter(f"runs/{prefix}")
+
+    lpips_score = Metric()
+    mse_score = Metric()
+    psnr_score = Metric()
+    content_loss_score = Metric()
+    loss_c_score = Metric(abs=True)
+    generator_loss_score = Metric()
+    adversarial_loss_score = Metric(abs=True)
+    gradient_norm_score = Metric()
+    generator_gradient_norm_score = Metric()
+
+    scores = {
+        "LPIPS": lpips_score,
+        "MSE": mse_score,
+        "PSNR": psnr_score,
+        "Content": content_loss_score,
+        "loss_C": loss_c_score,
+        "Generator": generator_loss_score,
+        "Adversarial": adversarial_loss_score,
+        "Critic Gradient Norm": gradient_norm_score,
+        "Generator Gradient Norm": generator_gradient_norm_score,
+    }
+
+    checkpoint_dict = {
+        "generator": trainer.generator,
+        "critic": trainer.critic,
+        "optimGenerator": trainer.optimGenerator,
+        "optimCritic": trainer.optimCritic,
+    } | scores
+
+    ep = load_checkpoint(checkpoint_dict, filename=f"{prefix}_checkpoint.pt")
+
+    for epoch in range(ep, ep + 6000):
+        bar = tqdm(dataloader)
+
+        for target in bar:
+            target = [t.cuda() for t in target]
+
+            result = trainer.train_batch(target, epoch)
+            scoresCritic, scoresGenerator = (result["critic"], result["generator"])
+            output = scoresGenerator["output"] if scoresGenerator is not None else None
+
+            if scoresCritic is not None:
+                gradient_norm_score.update(scoresCritic["gradient_norm"])
+                loss_c_score.update(scoresCritic["loss_c"])
+
+            if scoresGenerator is not None:
+                generator_gradient_norm_score.update(scoresGenerator["gradient_norm"])
+                content_loss_score.update(scoresGenerator["content_loss"])
+                adversarial_loss_score.update(scoresGenerator["adversarial_loss"])
+                generator_loss_score.update(scoresGenerator["loss"])
+
+                
+
+                bar.set_description(
+                    f"[{epoch+1}], Content: {content_loss_score.compute(reset=False):.3f}, loss_C: {loss_c_score.compute(reset=False):.3f}"
+                )
+                
+            log_metrics(writer, scores)
+
+        log_images(writer, trainer.generator, dataloader, epoch + 1)
+        save_checkpoint(
+            checkpoint_dict,
+            epoch + 1,
+            filename=f"{prefix}_checkpoint.pt",
+        )
+
+
+
+class Critic(nn.Module):
+    def __init__(self):
+        super(Critic, self).__init__()
+        # self.model256 = nn.Sequential(
+        #     nn.Conv2d(3, 32, kernel_size=9, stride=2, padding=4),
+        #     nn.LeakyReLU(inplace=True),  # 32x128x128
+        #     nn.Conv2d(32, 64, kernel_size=5, stride=2, padding=2),
+        #     nn.LeakyReLU(inplace=True),  # 64x64x64
+        #     nn.Conv2d(64, 128, kernel_size=5, stride=2, padding=2),
+        #     nn.LeakyReLU(inplace=True),  # 128x32x32
+        #     nn.Conv2d(128, 256, kernel_size=5, stride=2, padding=2),
+        #     nn.LeakyReLU(inplace=True),  # 256x16x16
+        #     nn.Conv2d(256, 512, kernel_size=5, stride=2, padding=2),
+        #     nn.LeakyReLU(inplace=True),  # 512x8x8
+        #     nn.Conv2d(512, 1024, kernel_size=5, stride=2, padding=2),
+        #     nn.LeakyReLU(inplace=True),  # 1024x4x4
+        #     nn.AvgPool2d(kernel_size=(4, 4)),  # 1024x1x1
+        #     nn.Flatten(),
+        #     nn.Linear(1024, 1, bias=False),  # Final output layer
+        # )
+
+        self.model64 = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=7, stride=2, padding=3),
+            nn.LeakyReLU(inplace=True),  # 32x128x128
+            nn.Conv2d(32, 64, kernel_size=5, stride=2, padding=2),
+            nn.LeakyReLU(inplace=True),  # 64x64x64
+            nn.Conv2d(64, 128, kernel_size=5, stride=2, padding=2),
+            nn.LeakyReLU(inplace=True),  # 128x32x32
+            nn.Conv2d(128, 256, kernel_size=5, stride=2, padding=2),
+            nn.LeakyReLU(inplace=True),  # 256x16x16
+            nn.AvgPool2d(kernel_size=(4, 4)),  # 1024x1x1
+            nn.Flatten(),
+            nn.Linear(256, 1, bias=False),  # Final output layer
+        )
+
+        for m in self.modules():
+          if isinstance(m, (nn.Conv2d, nn.Linear)):
+              nn.init.kaiming_normal_(m.weight, a=0.2)
+
+    def forward(self, x):
+       return self.model64(x)
+
